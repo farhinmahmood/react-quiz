3ffc0bd12d6b45a3d4f719401b6868a059df8c34
Saida Mahmood <saida.mahmood04@gmail.com>
2023-01-22 23:40:13 +0000

    Results page added

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,20 @@ import Layout from "./Layout";
 // import Home from './pages/Home'
 // import Signup from "./pages/Signup";
 // import Login from "./pages/Login";
-import Quiz from "./pages/Quiz";
+// import Quiz from "./pages/Quiz";
+import Results from "./pages/Results";
 
 //add lesson 3
 function App() {
   return (
    <Layout>
-    {/* <Home/> */}
-    {/* <Signup/> */}
-    {/* <Login/> */}
-    <Quiz/>
+      {/* <Home/> */}
+      {/* <Signup/> */}
+      {/* <Login/> */}
+      {/* <Quiz/> */}
+      <Results/>
    </Layout>
+
   );
 }
 
--- src/components/pages/Results.js
+++ src/components/pages/Results.js
@@ -1,9 +1,12 @@
-
+import Summary from '../Summary'
+import Analysis from '../Analysis'
 
 export default function Results(){
     return(
         
         <>
+            <Summary/>
+            <Analysis/>
         </>
             
 
