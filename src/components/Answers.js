074b653520bd036248dc3c4558cf88a6d3188de1
Saida Mahmood <saida.mahmood04@gmail.com>
2023-02-01 15:00:22 +0000

    question page setup with firebase

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -1,20 +1,19 @@
-import Checkbox from './Checkbox'
-import classes from '../styles/Answers.module.css'
+import classes from "../styles/Answers.module.css";
+import Checkbox from "./Checkbox";
 
-export default function Answers(){
-    return(
-        
-        <>
-            <div className={classes.answers}>
-                <Checkbox className={classes.answer}
-                text="Test Answer"
-                />
-            </div>
-            
-            
-        </>
-       
-
-       
-    )
+export default function Answers({ options = [], handleChange }) {
+  return (
+    <div className={classes.answers}>
+      {options.map((option, index) => (
+        <Checkbox
+          key={index}
+          className={classes.answer}
+          text={option.title}
+          value={index}
+          checked={option.checked}
+          onChange={(e) => handleChange(e, index)}
+        />
+      ))}
+    </div>
+  );
 }
